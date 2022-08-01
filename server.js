d4d49ec346543426c17686366527611ea2bc862d
Jin Kang <kbjss071@gmail.com>
2022-08-01 17:03:46 -0400

    worked on main homepage

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./controllers')
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(routes)
