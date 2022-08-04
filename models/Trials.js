5a2d8e43314708219c3fe20d7de6f9a0e1bf8f00
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-04 13:41:31 -0400

    adjusted challengs.hbs

--- models/Trials.js
+++ models/Trials.js
@@ -1,43 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Trial extends Model {}
 
 Trial.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        }, 
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            },
-        }, 
-        challenge_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        submission_detail: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }, 
-        status: {
-            type: DataTypes.BOOLEAN,
-            allowNull: true
-        }
-    }, 
-    {
-        sequelize,
-        freezeTableName: true,
-        tableName: 'trials',
-        underscored: true
-    }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    challenge_id: {
+      type: DataTypes.INTEGER,
+    },
+    submission_detail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    tableName: "trials",
+    underscored: true,
+  }
 );
 
-module.exports = Trial
+module.exports = Trial;
