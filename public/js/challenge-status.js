c27d870cc61afc7a965f1bcecabf56a1c68ed883
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-04 14:29:12 -0400

    challenge card js file

---
+++ public/js/challenge-status.js
@@ -0,0 +1,12 @@
+const challengeCard = [
+  { id: 1, challengeName: "Javascript" },
+  { id: 2, challengeName: "Handlebars" },
+  { id: 3, challengeName: "node.js" },
+  { id: 4, challengeName: "HTML" },
+  { id: 5, challengeName: "CSS" },
+  { id: 6, challengeName: "jquery" },
+  { id: 7, challengeName: "React" },
+  { id: 8, challengeName: "bootstrap" },
+  { id: 9, challengeName: "inquirer" },
+  { id: 10, challengeName: "npm" },
+];
